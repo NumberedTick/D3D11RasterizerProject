09a61b6a7b04b36ff53963849cb203dad99b74c9
NumberedTick <marcus.nordesjo@hotmail.com>
2024-07-11 13:35:51 +0200

    udated constant buffer .cpp file

--- RasterizerDemo/ConstantBufferD3D11.cpp
+++ RasterizerDemo/ConstantBufferD3D11.cpp
@@ -2,27 +2,70 @@
 
 ConstantBufferD3D11::ConstantBufferD3D11(ID3D11Device* device, size_t byteSize, void* initialData)
 {
+	this->bufferSize = byteSize;
 
+	Initialize(device, byteSize, initialData);
 }
 
 ConstantBufferD3D11::~ConstantBufferD3D11()
 {
-
+	if (buffer)
+	{
+		this->buffer->Release();
+		this->buffer = nullptr;
+	}
 }
-/*
-ConstantBufferD3D11::ConstantBufferD3D11(ConstantBufferD3D11&& other)
+
+ConstantBufferD3D11::ConstantBufferD3D11(ConstantBufferD3D11&& other) noexcept
+	: buffer(other.buffer), bufferSize(other.bufferSize)
 {
-
+	other.buffer = nullptr;
+	other.bufferSize = 0;
 }
 
-ConstantBufferD3D11& ConstantBufferD3D11::operator=(ConstantBufferD3D11&& other)
+ConstantBufferD3D11& ConstantBufferD3D11::operator=(ConstantBufferD3D11&& other) noexcept
 {
+	if (this != &other)
+	{
+		if (buffer)
+		{
+			this->buffer->Release();
+		}
+		this->buffer = other.buffer;
+		this->bufferSize = other.bufferSize;
 
+		other.buffer = nullptr;
+		other.bufferSize = 0;
+	}
+	return *this;
 }
-*/
+
 void ConstantBufferD3D11::Initialize(ID3D11Device* device, size_t byteSize, void* initialData)
 {
+	this->bufferSize = byteSize;
 
+	if (this->GetBuffer())
+	{
+		this->buffer->Release();
+		this->buffer = nullptr;
+	}
+
+	D3D11_BUFFER_DESC bufferDesc;
+	bufferDesc.ByteWidth = byteSize;
+	bufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+	bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bufferDesc.CPUAccessFlags = 0;
+	bufferDesc.MiscFlags = 0;
+	bufferDesc.StructureByteStride = 0;
+
+
+	// Data for the bufferDesc
+	D3D11_SUBRESOURCE_DATA data;
+	data.pSysMem = initialData;
+	data.SysMemPitch = 0;
+	data.SysMemSlicePitch = 0;
+
+	HRESULT hr = device->CreateBuffer(&bufferDesc, &data, &this->buffer);
 }
 
 size_t ConstantBufferD3D11::GetSize() const
